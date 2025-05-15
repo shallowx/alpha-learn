bb932b78aafcd6b8943b29ecf32bcaed840b4869
jiuyi.dream <jiuyi.dream@gmail.com>
2025-05-15 10:45:21 +0800

    Initialize the project, and support SpringBoot3.x + JDK-21

---
+++ src/main/java/com/alpha/learn/AlphaLearnApplication.java
@@ -0,0 +1,13 @@
+package com.alpha.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alpha/learn/AlphaLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
