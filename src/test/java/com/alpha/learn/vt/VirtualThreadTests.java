2e37320bb43be5cd608d0d736af19c9b2b511e7f
jiuyi.dream <jiuyi.dream@gmail.com>
2025-06-27 10:06:02 +0800

    virtual thread test

--- src/test/java/com/alpha/learn/vt/VirtualThreadTests.java
+++ src/test/java/com/alpha/learn/vt/VirtualThreadTests.java
@@ -2,6 +2,10 @@ package com.alpha.learn.vt;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
 public class VirtualThreadTests {
 
 
@@ -18,5 +22,17 @@ public class VirtualThreadTests {
         thread.start();
         System.out.println("--------------------");
         System.out.println(thread.isAlive());
+    }
+
+    @Test
+    public void test1() {
+        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            IntStream.range(0, 10_000).forEach(i -> {
+                executor.submit(() -> {
+                    Thread.sleep(Duration.ofMillis(100).toMillis());
+                    return i;
+                });
+            });
+        }
     }
 }
