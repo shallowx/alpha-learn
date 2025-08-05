c542bddd0258482b0adf083f041d6413f0f3f1cb
jiuyi.dream <jiuyi.dream@gmail.com>
2025-08-05 23:52:49 +0800

    add virtual threads tests

--- src/test/java/com/alpha/learn/features/VirtualThreadTests.java
+++ src/test/java/com/alpha/learn/features/VirtualThreadTests.java
@@ -4,7 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.lang.Thread.UncaughtExceptionHandler;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.*;
 
 @Slf4j
 public class VirtualThreadTests {
@@ -15,6 +15,31 @@ public class VirtualThreadTests {
         Thread t = virtualBuilder.name("virtualThread-test").uncaughtExceptionHandler(TestUncaughtExceptionHandler.INSTANCE)
                 .unstarted(new TestTask("test-task", ThreadLocalRandom.current().nextInt(0, 2)));
         t.start();
+    }
+
+    @Test
+    public void testPerVirtualThread() throws ExecutionException, InterruptedException {
+        final CountDownLatch countDownLatch = new CountDownLatch(2);
+        try (ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            Future<?> future = executor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    log.info("thread: {}", Thread.currentThread());
+                    countDownLatch.countDown();
+                }
+            });
+
+            Future<?> future1 = executor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    log.info("thread-1: {}", Thread.currentThread());
+                    countDownLatch.countDown();
+                }
+            });
+            countDownLatch.await();
+            System.out.println(future.isDone());
+            System.out.println(future1.isDone());
+        }
     }
 
     record TestTask(String name, int id) implements Runnable {
