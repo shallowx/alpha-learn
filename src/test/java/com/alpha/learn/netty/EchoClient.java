7a899a17449bb67f95950dd68b89814fbb1ddb1a
jiuyi.dream <jiuyi.dream@gmail.com>
2025-08-29 22:39:45 +0800

    Netty test unit

---
+++ src/test/java/com/alpha/learn/netty/EchoClient.java
@@ -0,0 +1,76 @@
+package com.alpha.learn.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioIoHandler;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.CharsetUtil;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+public class EchoClient {
+
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup group = new MultiThreadIoEventLoopGroup(NioIoHandler.newFactory());
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new  ChannelInitializer<SocketChannel>() {
+
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast(new EchoClientHandler());
+                        }
+                    });
+            ChannelFuture f = bootstrap.connect("127.0.0.1", 8000).sync();
+            f.addListener((ChannelFutureListener) future -> {
+                if (future.isSuccess()) {
+                    log.info("Server connected to Echo server successfully...");
+                } else  {
+                    log.info("Server connected to Echo server failed...");
+                }
+            });
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+
+   static class EchoClientHandler extends ChannelInboundHandlerAdapter {
+        private final ByteBuf firstMessage;
+        public EchoClientHandler() {
+            firstMessage = Unpooled.buffer(256);
+            for (int i = 0; i < firstMessage.capacity(); i ++) {
+                firstMessage.writeByte((byte) i);
+            }
+        }
+
+        @Override
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            ctx.writeAndFlush(firstMessage);
+        }
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            ByteBuf buf = (ByteBuf) msg;
+            log.info("Client received echo: {}", buf.toString(CharsetUtil.UTF_8));
+            ByteBuf buffer = ctx.alloc().buffer();
+            buffer.writeBytes("Hello Server".getBytes(StandardCharsets.UTF_8));
+            ctx.write(buffer);
+            buf.release();
+        }
+
+        @Override
+        public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+            ctx.flush();
+        }
+    }
+}
---
+++ src/test/java/com/alpha/learn/netty/EchoServer.java
@@ -0,0 +1,74 @@
+package com.alpha.learn.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioIoHandler;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.util.CharsetUtil;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+public class EchoServer {
+    public static void main(String[] args) {
+        EventLoopGroup bossGroup = new MultiThreadIoEventLoopGroup(NioIoHandler.newFactory());
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    .handler(new LoggingHandler(LogLevel.DEBUG))
+                    .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                        @Override
+                        public void initChannel(NioSocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new EchoServerHandler());
+                        }
+                    });
+            ChannelFuture future = bootstrap.bind(8000).sync();
+            future.addListener((ChannelFutureListener) f -> {
+                if (f.isSuccess()) {
+                    log.info("Server started on port 8000");
+                } else  {
+                    log.error("Server start failed");
+                }
+            });
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            log.error("Server exception", e);
+            throw new RuntimeException(e);
+        } finally {
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    static class EchoServerHandler extends ChannelInboundHandlerAdapter {
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            ByteBuf buf = (ByteBuf) msg;
+            log.info("Server receive message from client: {}", buf.toString(StandardCharsets.UTF_8));
+            ByteBuf buffer = ctx.alloc().buffer();
+            buffer.writeBytes(("Hello Client").getBytes(CharsetUtil.UTF_8));
+            ctx.write(buffer);
+            buf.release();
+        }
+
+        @Override
+        public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+            ctx.flush();
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            log.error("Server exception", cause);
+            cause.printStackTrace();
+            ctx.close();
+        }
+    }
+}
